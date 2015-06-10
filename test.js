2188c96b31bac4bc1ca62737a14c031ae8f3743c
Casper Beyer <caspervonb@qup.io>
2015-06-11 05:14:58 +0800

    Remover FF test, make sure there's no bleedover

--- test.js
+++ test.js
@@ -2,33 +2,33 @@ var child = require('child_process');
 var test = require('tape');
 var http = require('http');
 
-
-test('firefox', function(test) {
-  var server = http.createServer();
-
-  server.on('listening', function() {
-    var ps = child.spawn('firefox', ['http://localhost:8000/']);
-
-    ps.on('close', function(code, signal) {
-      console.log('browser closed %s %s', code, signal);
-    });
-
-    ps.stderr.pipe(process.stderr);
-    ps.stdout.pipe(process.stderr);
-
-    server.on('request', function(request, response) {
-      test.comment(request.url);
-
-      if (request.url === '/') {
-        ps.kill();
-        server.close();
-        test.end();
-      }
-    });
-  });
-
-  server.listen(8000);
-});
+// 
+// test('firefox', function(test) {
+//   var server = http.createServer();
+//
+//   server.on('listening', function() {
+//     var ps = child.spawn('firefox', ['http://localhost:8000/']);
+//
+//     ps.on('close', function(code, signal) {
+//       console.log('browser closed %s %s', code, signal);
+//     });
+//
+//     ps.stderr.pipe(process.stderr);
+//     ps.stdout.pipe(process.stderr);
+//
+//     server.on('request', function(request, response) {
+//       test.comment(request.url);
+//
+//       if (request.url === '/') {
+//         ps.kill();
+//         server.close();
+//         test.end();
+//       }
+//     });
+//   });
+//
+//   server.listen(8000);
+// });
 
 test('chrome', function(test) {
   var server = http.createServer();
