87251bdd7fc3829e8e3704f44ea949fa8d0bf98d
Casper Beyer <caspervonb@qup.io>
2015-06-11 04:14:07 +0800

    Add firefox test

--- test.js
+++ test.js
@@ -2,16 +2,40 @@ var child = require('child_process');
 var test = require('tape');
 var http = require('http');
 
-test('chrome', function(test) {
+
+test('firefox', function(test) {
+  test.timeoutAfter(15000);
   var server = http.createServer();
 
   server.on('listening', function() {
-    var ps = child.spawn('/usr/bin/google-chrome', ['http://localhost:8000']);
+    var ps = child.spawn('firefox', ['http://localhost:8000/']);
 
     ps.on('close', function(code, signal) {
       console.log('browser closed %s %s', code, signal);
     });
-    
+
+    ps.stderr.pipe(process.stderr);
+    ps.stdout.pipe(process.stderr);
+
+    server.on('request', function() {
+      test.end('request made');
+    });
+  });
+
+  server.listen(8000);
+});
+
+test('chrome', function(test) {
+  test.timeoutAfter(15000);
+  var server = http.createServer();
+
+  server.on('listening', function() {
+    var ps = child.spawn('google-chrome', ['http://localhost:8000/']);
+
+    ps.on('close', function(code, signal) {
+      console.log('browser closed %s %s', code, signal);
+    });
+
     ps.stderr.pipe(process.stderr);
     ps.stdout.pipe(process.stderr);
 
