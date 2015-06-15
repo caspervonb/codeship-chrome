0631f53a20cee2ed0fbd699b68700de060160837
Casper Beyer <caspervonb@qup.io>
2015-06-16 07:46:22 +0800

    Simplify test case, prepping for support@codeship

--- test.js
+++ test.js
@@ -2,53 +2,27 @@ var child = require('child_process');
 var test = require('tape');
 var http = require('http');
 
-// 
-// test('firefox', function(test) {
-//   var server = http.createServer();
-//
-//   server.on('listening', function() {
-//     var ps = child.spawn('firefox', ['http://localhost:8000/']);
-//
-//     ps.on('close', function(code, signal) {
-//       console.log('browser closed %s %s', code, signal);
-//     });
-//
-//     ps.stderr.pipe(process.stderr);
-//     ps.stdout.pipe(process.stderr);
-//
-//     server.on('request', function(request, response) {
-//       test.comment(request.url);
-//
-//       if (request.url === '/') {
-//         ps.kill();
-//         server.close();
-//         test.end();
-//       }
-//     });
-//   });
-//
-//   server.listen(8000);
-// });
-
 test('chrome', function(test) {
   var server = http.createServer();
 
   server.on('listening', function() {
+    test.comment('server listening');
+
     var ps = child.spawn('google-chrome', ['http://localhost:8000/']);
 
     ps.on('close', function(code, signal) {
-      console.log('browser closed %s %s', code, signal);
+      test.comment('browser closed');
+      server.close();
     });
 
     ps.stderr.pipe(process.stderr);
     ps.stdout.pipe(process.stderr);
 
     server.on('request', function(request, response) {
-      test.comment(request.url);
+      test.comment('server request ' + request.url);
 
       if (request.url === '/') {
         ps.kill();
-        server.close();
         test.end();
       }
     });
